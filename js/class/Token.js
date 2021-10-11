3ca39af847f79ddc2043b0a0d9eb52670475530e
Federico Fuhr <46513550+federicofuhr@users.noreply.github.com>
2021-10-10 21:07:55 -0300

    agregados comentarios en la clase Token

--- js/class/Token.js
+++ js/class/Token.js
@@ -1,6 +1,9 @@
 import Drawable from "./Drawable.js";
 
 export default class Token extends Drawable {
+    /**
+     * CONSTRUCTOR DE LA CLASE
+     */
     constructor(posX, posY, lastPosX, lastPosY, radius, context, playerId, imgPath) {
         super(posX, posY, lastPosX, lastPosY, context);
         this.radius = radius;
@@ -11,12 +14,21 @@ export default class Token extends Drawable {
         this.img.onload = this.draw();
         this.locked = false;
     }
+    /**
+     * DEVUELVE UN BOOLEAN QUE DICE SI LA FICHA ESTA BLOQUEADA O NO
+     */
     isLocked() {
         return this.locked;
     }
+    /**
+     * CAMBIA EL ESTADO DE BLOQUEADO DE LA FICHA
+     */
     setLocked(status) {
         this.locked = status;
     }
+    /**
+     * FUNCION PARA DIBUJAR LA FICHA EN EL CANVAS CON LA IMAGEN
+     */
     draw() {
         this.ctx.save();
         this.ctx.beginPath();
@@ -40,17 +52,29 @@ export default class Token extends Drawable {
         this.ctx.closePath();
         this.ctx.restore();
     }
+    /**
+     * RETORNA EL RADIO DE LA FICHA
+     */
     getRadius() {
         return this.radius;
     }
+    /**
+     * RETORNA EL ID DEL JUGADOR
+     */
     getPlayerId() {
         return this.playerId;
     }
+    /**
+     * RETORNA UN BOOLEAN QUE DICE SI LA FICHA ESTA EN LAS COORDENADAS X e Y
+     */
     isPointInside(x, y) {
         const _x = this.posX - x;
         const _y = this.posY - y;
         return (Math.sqrt(_x * _x + _y * _y) < this.radius);
     }
+    /**
+     * CARGA LA IMAGEN POR EL PATH PASADO POR PARAMETRO
+     */
     setImage(imgPath) {
         let img = new Image();
         img.src = './images/' + imgPath;
