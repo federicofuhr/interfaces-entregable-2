e015364d81b902b6b736f26d9c9f77e54ada45c8
Federico Fuhr <46513550+federicofuhr@users.noreply.github.com>
2021-10-10 21:36:10 -0300

    agregados comentarios en board, cell y drawable

--- js/class/Board.js
+++ js/class/Board.js
@@ -1,5 +1,9 @@
 import Cell from "./Cell.js";
+
 export default class Board {
+    /**
+     * CONSTRUCTOR DE LA CLASE
+     */
     constructor(posX, posY, rows, cols, cellSize, context) {
         this.posX = posX;
         this.posY = posY;
@@ -9,7 +13,9 @@ export default class Board {
         this.context = context;
         this.board = this.createBoard();
     }
-
+    /**
+     * CREA EL TABLERO POR UNA MATRIZ DONDE CADA CASILLERO TIENE UNA CELDA
+     */
     createBoard() {
         let cx = this.posX;
         let cy = this.posY;
@@ -26,52 +32,93 @@ export default class Board {
         }
         return matrix;
     }
-    // Getters
+    /**
+     * RETORNA LA CELDA EN LA POSICION I y J
+     */
     getCell(i, j) {
         return this.board[i][j];
     }
+    /**
+     * RETORNA LA COORDEANDA X
+     */
     getPosX() {
         return this.posX;
     }
+    /**
+     * RETORNA LA COORDENADA Y
+     */
     getPosY() {
         return this.posY;
     }
+    /**
+     * RETORNA LA CANTIDAD DE FILAS
+     */
     getRows() {
         return this.rows;
     }
+    /**
+     * RETORNA LA CANTIDAD DE COLUMNAS
+     */
     getCols() {
         return this.cols;
     }
+    /**
+     * RETORNA EL ANCHO/ALTO DE LA CELDA
+     */
     getCellSize() {
         return this.cellSize;
     }
+    /**
+     * RETORNA EL CONTEXTO DEL TABLERO
+     */
     getContext() {
         return this.context;
     }
+    /**
+     * RETORNA EL TABLERO
+     */
     getBoard() {
         return this.board;
     }
-    // Setters
+    /**
+     * INGRESA LA COORDENADA X
+     */
     setPosX(x) {
         this.posX = x;
     }
+    /**
+     * INGRESA LA COORDENADA Y
+     */
     setPosY(y) {
         this.posY = y;
     }
+    /**
+     * INGRESA LA CANTIDAD DE FILAS
+     */
     setRows(r) {
         this.rows = r;
     }
+    /**
+     * INGRESA LA CANTIDAD DE COLUMNAS
+     */
     setCols(c) {
         this.cols = c;
     }
+    /**
+     * INGRESA EL ANCHO/ALTO DE LA CELDA
+     */
     setCellSize(s) {
         this.cellSize = s;
     }
+    /**
+     * INGRESO EL CONTEXTO
+     */
     setContext(context) {
         this.context = context;
     }
-
-    // Funciones para representar el tablero en el canvas
+    /**
+     * FUNCION PARA DIBUJAR EL TABLERO EN EL CANVAS
+     */
     drawBoard() {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
--- js/class/Drawable.js
+++ js/class/Drawable.js
@@ -1,4 +1,7 @@
 export default class Drawable {
+    /**
+     * CONSTRUCTOR DE LA CLASE
+     */
     constructor(posX, posY, lastPosX, lastPosY, context) {
         this.posX = posX;
         this.posY = posY;
@@ -8,14 +11,23 @@ export default class Drawable {
         this.lastPosX = lastPosX;
         this.lastPosY = lastPosY;
     }
+    /**
+     * SE PASAN LA ULTIMA POSICION VALIDA EN EL CANVAS
+     */
     setLastPosition(x, y) {
         this.lastPosX = x;
         this.lastPosY = y;
     }
+    /**
+     * SE CARGAN LAS COORDENADAS DEL CANVAS PASADAS POR PARAMETRO
+     */
     setPosition(x, y) {
         this.posX = x;
         this.posY = y;
     }
+    /**
+     * RETORNA LAS COORDENADAS DE LA ULTIMA POSICION VALIDA
+     */
     getLastPosition() {
         const result = {
             x: this.lastPosX,
@@ -23,6 +35,9 @@ export default class Drawable {
         };
         return result;
     }
+    /**
+     * RETORNA LAS COORDENADAS ACTUALES
+     */
     getPosition() {
         const result = {
             x: this.posX,
@@ -30,21 +45,39 @@ export default class Drawable {
         };
         return result;
     }
+    /**
+     * RETORNA LA COORDENADA X
+     */
     getPosX() {
         return this.posX;
     }
+    /**
+     * RETORNA LA COORDENADA Y
+     */
     getPosY() {
         return this.posY;
     }
+    /**
+     * RETORNA LA ULTIMA COORDENADA X VALIDA
+     */
     getLastPosX() {
         return this.lastPosX;
     }
+    /**
+     * RETORNA LA ULTIMA COORDENADA Y VALIDA
+     */
     getLastPosY() {
         return this.lastPosY;
     }
+    /**
+     * SE PASA POR PARAMETRO SI ES RESALTADO
+     */
     setResaltado(resaltado) {
         this.resaltado = resaltado;
     }
+    /**
+     * FUNCION ABSTRACTA PARA SABER SI SE ENCUENTRA EN LAS COORDENADAS X e Y
+     */
     isPointInside(x, y) {
         
     }
